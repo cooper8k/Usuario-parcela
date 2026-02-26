5b32e0686a8004a1e34f45c16edd0eafb9feedd5
cooper8k <orlanprado.c@gmail.com>
2026-02-25 22:16:23 -0300

    config security

--- src/main/java/com/perfil/user/config/SecurityConfig.java
+++ src/main/java/com/perfil/user/config/SecurityConfig.java
@@ -3,24 +3,22 @@ package com.perfil.user.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/api/v1/user/login", "/api/v1/user/registrar", "/api/v1/user/eliminar/**").permitAll()
-            .anyRequest().authenticated()
-        );
+            .cors(cors -> cors.configure(http)) // usa el CorsFilter que ya tienes
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/user/registrar", "/api/v1/user/login").permitAll()
+                .anyRequest().authenticated()
+            );
         return http.build();
     }
 }
-// Configuración de seguridad para la aplicación utilizando Spring Security.
-// La anotación @Configuration indica que esta clase contiene configuraciones de Spring.
-// El método filterChain define la cadena de filtros de seguridad para las solicitudes HTTP.
-// Se desactiva la protección CSRF para simplificar las pruebas (no recomendado para producción).
-// Se permite el acceso sin autenticación a las rutas de login y registro, mientras que todas las demás rutas requieren autenticación.
